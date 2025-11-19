a6f429a4f5d62268b7cd776ff9e56ba620a1ac8b
Fabio Giugni <fabiogiugni@ufmg.br>
2025-11-19 13:20:47 -0300

    add: settings.py

---
+++ utils/settings.py
@@ -0,0 +1,34 @@
+import pygame
+
+# Screen Settings
+FULLSCREEN = True  # Set to True for fullscreen mode
+SCREEN_WIDTH = 800  # Default width (used when not in fullscreen)
+SCREEN_HEIGHT = 600  # Default height (used when not in fullscreen)
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+CYAN = (0, 255, 255)
+GREY = (128, 128, 128)
+
+# Game Settings
+GAME_TITLE = "Snake Game"
+
+# Snake Settings
+SNAKE_SPEED = 15 #NÃO CONFIRMADO
+SNAKE_SIZE = 15 #NÃO CONFIRMADO
+
+# Apple Settings
+
+
+# Block Settings
+
+
+BLOCK_SIZE = 15 #NÃO CONFIRMADO
