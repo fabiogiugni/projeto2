28f866f42673d651e7790855e8fd3e638de311ec
Thales <thales@thales.Thales>
2025-11-19 23:08:31 -0300

    feat: snake.grow

--- entities/block.py
+++ entities/block.py
@@ -1,17 +1,59 @@
+import pygame
 import pygame.display
 from pygame import Surface
 
+from utils.settings import BLOCK_SIZE
+
 class Block(Surface):
-    def __init__(self, size : tuple = (15, 15), position : tuple = (50, 50)):
+    def __init__(self, size : tuple = (BLOCK_SIZE, BLOCK_SIZE), position : tuple = (50, 50), image_path: str = None):
         super().__init__(size)
-        
         self._position = position
+        self._image = None
         
+        # Load image if provided
+        if image_path:
+            self.load_image(image_path, size)
+
+    def load_image(self, image_path: str, size: tuple = None):
+        """Load and scale an image for the block"""
+        try:
+            # Load the image
+            original_image = pygame.image.load(image_path).convert_alpha()
+            
+            # Scale to desired size if provided, otherwise use original size
+            if size:
+                size = (size[0] * 2 , size[1]*2)
+                self._image = pygame.transform.scale(original_image, size)
+            else:
+                self._image = original_image
+            
+            print(image_path)
+            print(self._image)
+            
+            # Update the surface size to match the image
+            super().__init__(self._image.get_size())
+            
+            
+        except pygame.error as e:
+            print(f"Unable to load image: {image_path}")
+            print(e)
+            self._image = None
 
     def fillColor(self, color : tuple):
         super().fill(color)
 
-    def draw(self, display : pygame.display):
-        display.blit(self, self._position)
+    def draw(self, display: pygame.display):
+        """Draw the block - image takes priority over color"""
+        if self._image:
+            display.blit(self._image, (self._position[0]-5, self._position[1] -5))
+        else:
+            display.blit(self, self._position)
+        
 
+    @property
+    def position(self):
+        return self._position
     
+    @position.setter
+    def position(self, position : tuple):
+        self._position = position
--- entities/snake.py
+++ entities/snake.py
@@ -1,36 +1,56 @@
 import pygame
 from pygame import Surface
+from .block import Block
 from .snakeBlock import SnakeBlock
 
+from utils.settings import PURPLE, CYAN, GREEN, DARK_GREEN
+
 class Snake:
-    def __init__(self, displayRange : tuple, position : tuple = (100, 100)):
+    def __init__(self, displayRange : tuple, position : tuple = (45, 45)):
         self.__position = position
         self.__size = 3
-        self.__blocks = [SnakeBlock((50, 80)), SnakeBlock((50, 65)), SnakeBlock((50, 50))]
+        self.__blocks = [SnakeBlock((45, 60), image_path="assets/snake.png"), SnakeBlock((45, 45)), SnakeBlock((45, 30))]
         self.__direction = "DOWN"
         for b in self.__blocks:
+            b.fillColor(GREEN)
             b.turn(self.__direction)
 
     def draw(self, display : pygame.display):
         for b in self.__blocks:
-            b.fillColor((0, 255, 0))
             b.draw(display)
 
     def turn(self, direction : str):
+        if direction == "LEFT" and self.__direction == "RIGHT":
+            self.die()
+        if direction == "RIGHT" and self.__direction == "LEFT":
+            self.die()
+        if direction == "DOWN" and self.__direction == "UP":
+            self.die()
+        if direction == "UP" and self.__direction == "DOWN":
+            self.die()
+
         self.__direction = direction
-        
-                
+          
 
     def walk(self):
-        stop = False
-        for b in self.__blocks:
-            if (not stop) and b.direction != self.__direction:
-                b.turn(self.__direction)
-                stop = True
-        for b in self.__blocks:
-            b.walk()
+
+        for i in range(1, len(self.__blocks)):
+            self.__blocks[len(self.__blocks) - i].position = self.__blocks[len(self.__blocks) - i - 1].position
+
+        self.__blocks[0].turn(self.__direction)
+        self.__blocks[0].walk()
 
     def die(self):
-        pass
-        
-        
+        for b in self.__blocks:
+            b.fillColor(CYAN)
+
+    def grow(self):
+        new_block = SnakeBlock((-15, -15))
+        new_block.fillColor(GREEN)
+        self.__blocks.append(new_block)
+
+    def colide(self, block : Block):
+        if block.position == self.__blocks[0].position:
+            return True
+        else:
+            return False
